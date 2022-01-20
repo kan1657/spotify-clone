eac2fd6c8485193428cde5913001b3c3e118069c
kan1657 <kan1657@hotmail.com>
2022-01-20 09:20:04 +0700

    fix login flow and add middleware js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import 'tailwindcss/tailwind.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
+-import 'tailwindcss/tailwind.css'
++import "tailwindcss/tailwind.css";
++import { SessionProvider } from "next-auth/react";
+ 
+-function MyApp({ Component, pageProps }) {
+-  return <Component {...pageProps} />
++function MyApp({ Component, pageProps: { session, ...pageProps } }) {
++  return (
++    <SessionProvider session={session}>
++      <Component {...pageProps} />
++    </SessionProvider>
++  );
+ }
+ 
+-export default MyApp
++export default MyApp;
